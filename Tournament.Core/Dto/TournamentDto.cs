919e1c178aac6c40afba125ccf74b1cc2c63f2c5
Mohammad Abdulkader <mohammad.andulkader9@outlook.com>
2024-11-28 16:14:46 +0100

    added Dtos for Tournament and Game

---
+++ Tournament.Core/Dto/TournamentDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    public record TournamentDto
+    {
+        public int Id { get; init; }
+        public string? Title { get; init; }
+        public DateTime? StartDate { get; init; }
+        public DateTime? EndDate { get; init; }
+
+        public TournamentDto(int id, string? title, DateTime? startDate)
+        {
+            Id = id;
+            Title = title;
+            StartDate = startDate;
+            EndDate = startDate?.AddMonths(3);
+        }
+    }
+}
