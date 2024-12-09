0cb8f06e601bdcbaecd0c82be31ce40693c4d00c
Mohammad Abdulkader <mohammad.andulkader9@outlook.com>
2024-12-09 09:48:20 +0100

    Added RepositoryBase

---
+++ Tournament.Core/Repositories/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Repositories
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IQueryable<T> FindAll(bool trackChanges = false);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false);
+
+        void Add(T entity);
+        void Remove(T entity);
+        void Update(T entity);
+    }
+}
---
+++ Tournament.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Repositories;
+using Tournament.Data.Data;
+
+namespace Tournament.Data.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected TournamentContext Context { get; }
+        protected DbSet<T> DbSet { get; }
+
+        public RepositoryBase(TournamentContext context)
+        {
+            Context = context;
+            DbSet = context.Set<T>();
+            
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges = false) =>
+                        trackChanges ? DbSet :
+                                       DbSet.AsNoTracking();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false) =>
+                        trackChanges ? DbSet.Where(expression) :
+                                       DbSet.Where(expression).AsNoTracking();
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
